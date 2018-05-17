37e7a19a03eab48732824a64c55c580f0f89cbe9
Dillon L. Peterson <dillonpeterson@mac.com>
2018-05-17 11:36:36 -0500

    Final initial implementation for vessel-service tutorial2

---
+++ Dockerfile
@@ -0,0 +1,42 @@
+# We use the official golang image, which contains all the 
+# xorrect build tools and libraries. Notice 'as builder',
+# this gives the container a name that we can reference later on.
+FROM golang:1.9.0 as builder 
+
+# Set our workdir to our current service in the gopath
+WORKDIR /go/src/bitbucket.org/dillonlpeterson/shippy/vessel-service
+
+# Copy the current directory into our workdir 
+COPY . .
+
+# Here we are pulling in godep, which is a dependency management tool
+# We're going to use godep instead of go get (Gets fussy with sub-packages sometimes)
+RUN go get -u github.com/golang/dep/cmd/dep
+
+# Create a dep project, and run 'ensure', which will pull in all of the 
+# dependencies of this directory.
+RUN dep init && dep ensure 
+
+# Build the binary, with a few flags that will allow us 
+# to run the binary in Alpine. 
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo .
+
+# Here we're using a second FROM statement, which tells Docker to start 
+# a new build process with this image
+FROM alpine:latest 
+
+# Security-related package good to have 
+RUN apk --no-cache add ca-certificates
+
+RUN mkdir /app 
+WORKDIR /app 
+
+#ADD consignment-service /app/consignment-service 
+# Instead of pulling binary from Host machine, we pill it from the container named builder!
+# Copies into /app directory, which is the current working directory.
+COPY --from=builder /go/src/bitbucket.org/dillonlpeterson/shippy/vessel-service/vessel-service .
+
+# As usual, run the binary!
+CMD ["./consignment-service"]
+
+# Code must be pushed up to Git so that it can pull in other services.
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+
+	pb "bitbucket.org/dillonlpeterson/shippy/vessel-service/proto/vessel"
+	micro "github.com/micro/go-micro"
+	"golang.org/x/net/context"
+)
+
+type Repository interface {
+	FindAvailable(*pb.Specification) (*pb.Vessel, error)
+}
+
+type VesselRepository struct {
+	vessels []*pb.Vessel
+}
+
+// FindAvailable - checks a specification against a map of vessels ,
+// if capacity amd max weight are below a vessel's capacity and max weight,
+// then return that vessel
+func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
+	for _, vessel := range repo.vessels {
+		if spec.Capacity <= vessel.Capacity && spec.MaxWeight <= vessel.MaxWeight {
+			return vessel, nil
+		}
+	}
+	return nil, errors.New("No vessel found by that spec.s")
+}
+
+// our grpc service handler
+type service struct {
+	repo Repository
+}
+
+func (s *service) FindAvailable(ctx context.Context, req *pb.Specification, res *pb.Response) error {
+	vessel, err := s.repo.FindAvailable(req)
+	if err != nil {
+		return err
+	}
+
+	// Set the vessel as part of the response message type
+	res.Vessel = vessel
+	return nil
+}
+
+func main() {
+	vessels := []*pb.Vessel{
+		&pb.Vessel{Id: "vessel001", Name: "Boaty Mcboatface", MaxWeight: 200000, Capacity: 500},
+	}
+	repo := &VesselRepository{vessels}
+
+	srv := micro.NewService(
+		micro.Name("go.micro.srv.vessel"),
+		micro.Version("latest"),
+	)
+
+	srv.Init()
+
+	// Register our implementation with
+	pb.RegisterVesselServiceHandler(srv.Server(), &service{repo})
+
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
+	}
+}
