ed3b4652c727209fddfb08883e024311fb295b6a
Dillon L. Peterson <dillonpeterson@mac.com>
2018-05-27 20:08:43 -0500

    FIXED BUG

--- repository.go
+++ repository.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"log"
-
 	pb "github.com/dillonlpeterson/shippy-vessel-service/proto/vessel"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
@@ -28,19 +26,17 @@ type VesselRepository struct {
 // then return that vessel
 func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
 	var vessel *pb.Vessel
-	log.Println("GERE WUFFH FHHFH")
 	// Here we define a more complex query than our consignment-service's
 	// GetAll function. Here we're asking for a vessel who's max weight and
 	// capacity are greater than and equal to the given capacity and weight.
 	// We're also using the `One` function here as that's all we want.
 	err := repo.collection().Find(bson.M{
 		"capacity":  bson.M{"$gte": spec.Capacity},
-		"maxweight": bson.M{"gte": spec.MaxWeight},
+		"maxweight": bson.M{"$gte": spec.MaxWeight},
 	}).One(&vessel)
 	if err != nil {
 		return nil, err
 	}
-	log.Println("Found a vessel")
 	return vessel, err
 }
 
